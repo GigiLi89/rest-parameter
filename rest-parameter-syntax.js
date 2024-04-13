620fbb6532467d44c9e6dbd699ac39a3271f27bd
Gigi Li <160052489+GigiLi89@users.noreply.github.com>
2024-04-13 09:43:29 +0200

    Initial commit

---
+++ rest-parameter-syntax.js
@@ -0,0 +1,10 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node rest-parameter-syntax.js in the terminal
+ */
+
+// Regular function call 
+
+// Extra arguments are ignored
+
+// Function using ...rest
